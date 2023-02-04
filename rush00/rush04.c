132b3624091bfc2fc76e77fc97237a9158554106
Léo <leogaudinoff@gmail.com>
2023-02-04 19:12:06 +0100

    Replace end of loop increments by ++

--- rush00/rush04.c
+++ rush00/rush04.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 12:07:55 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/04 19:10:30 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/04 19:11:46 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@ void	rush(int x, int y)
 
 	i = 1;
 	j = 1;
-	while (j <= y)
+	while (j++ <= y)
 	{
-		while (i <= x)
+		while (i++ <= x)
 		{
 			if ((i == 1 && j == 1) || (i == x && j == y && y != 1 && x != 1))
 				ft_putchar('A');
@@ -34,10 +34,8 @@ void	rush(int x, int y)
 				ft_putchar('B');
 			else
 				ft_putchar(' ');
-			i = i + 1;
 		}
 		i = 1;
-		j = j + 1;
 		ft_putchar('\n');
 	}
 }
