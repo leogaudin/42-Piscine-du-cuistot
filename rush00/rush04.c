7633306f11672cb38994e6f7190374d40cbd7116
lgaudin <lgaudin@student.42malaga.com>
2023-02-04 13:02:50 +0100

    Add rush04

---
+++ rush00/rush04.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 12:07:55 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/04 12:55:37 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 1;
+	while (j <= y)
+	{
+		while (i <= x)
+		{
+			if ((i == 1 && j == 1) || (i == x && j == y && y != 1 && x != 1))
+				ft_putchar('A');
+			else if ((i == x && j == 1) || (i == 1 && j == y))
+				ft_putchar('C');
+			else if (j == 1 || j == y || i == 1 || i == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			i = i + 1;
+		}
+		i = 1;
+		j = j + 1;
+		ft_putchar('\n');
+	}
+}
