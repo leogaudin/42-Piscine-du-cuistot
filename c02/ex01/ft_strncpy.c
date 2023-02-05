34fbd11b8f753914427856c19dd87088e16b033b
lgaudin <lgaudin@student.42malaga.com>
2023-02-05 20:46:19 +0100

    Add different approach

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,26 +6,27 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:11:28 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 20:35:15 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 20:45:48 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-	unsigned int	dest_size;
 
-	dest_size = 0;
-	while (dest[dest_size])
-		dest_size++;
 	i = 0;
-	if (n > dest_size + 1)
-		return (0);
-	while (i < n - 1 && src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
-		i++;
+		++i;
 	}
-	dest[i] = src[i];
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			++i;
+		}
+	}
 	return (dest);
 }
--- c02/mains/ex01/ft_strncpy.c
+++ c02/mains/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:11:28 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 20:35:06 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 20:45:34 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,21 @@
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
-	unsigned int	dest_size;
 
-	dest_size = 0;
-	while (dest[dest_size])
-		dest_size++;
 	i = 0;
-	if (n > dest_size + 1)
-		return (0);
-	while (i < n - 1 && src[i] != '\0')
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
-		i++;
+		++i;
 	}
-	dest[i] = src[i];
+	if (i < n && src[i] == '\0')
+	{
+		while (dest[i] != '\0')
+		{
+			dest[i] = '\0';
+			++i;
+		}
+	}
 	return (dest);
 }
 
@@ -39,7 +40,7 @@ int main(void)
 	char second[] = "Hello";
 	printf("First string is %s\n", first);
 	printf("Second string is %s\n\n", second);
-	printf("ft_strncpy returned %s\n\n", strncpy(first, second, 6));
+	printf("ft_strncpy returned %s\n\n", ft_strncpy(first, second, 6));
 	printf("First string is %s\n", first);
 	printf("Second string is %s\n\n", second);
 	return 0;
