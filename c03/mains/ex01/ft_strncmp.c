0742613c09cfb87432ba7f0bda0c505a2d2fced9
lgaudin <lgaudin@student.42malaga.com>
2023-02-05 18:52:28 +0100

    Add ex01 and its tester

--- c03/mains/ex01/ft_strncmp.c
+++ c03/mains/ex01/ft_strncmp.c
@@ -6,9 +6,12 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 17:31:55 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 18:49:51 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 18:52:09 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
@@ -23,4 +26,13 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 			i++;
 	}
 	return (0);
+}
+
+int main(int argc, char const *argv[])
+{
+	char first[] = "Bonjourrrrr";
+	char second[] = "Bonjourrrre";
+	printf("strcmp returned %d\n", strncmp(first, second, 10));
+	printf("ft_strcmp returned %d\n", ft_strncmp(first, second, 10));
+	return 0;
 }
