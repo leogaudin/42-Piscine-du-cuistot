fc0d0f7732bf1a7de17d46b32a0f4b3532277472
lgaudin <lgaudin@student.42malaga.com>
2023-02-05 20:31:14 +0100

    Add corrections for ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,15 +6,21 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:11:28 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 18:14:40 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 20:30:56 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
+	unsigned int	dest_size;
 
+	dest_size = 0;
+	while (dest[dest_size])
+		dest_size++;
 	i = 0;
+	if (n > dest_size + 1)
+		// return (NULL);
 	while (i < n - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
--- c02/mains/ex01/ft_strncpy.c
+++ c02/mains/ex01/ft_strncpy.c
@@ -6,17 +6,24 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:11:28 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 18:33:58 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 20:30:45 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <string.h>
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
+	unsigned int	dest_size;
 
+	dest_size = 0;
+	while (dest[dest_size])
+		dest_size++;
 	i = 0;
+	if (n > dest_size + 1)
+		// return (NULL);
 	while (i < n - 1 && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -26,13 +33,13 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	return (dest);
 }
 
-int main(int argc, char const *argv[])
+int main(void)
 {
-	char first[] = "Hello";
-	char second[] = "Bonjour";
+	char first[] = "Bonjour salut ca va";
+	char second[] = "Hello";
 	printf("First string is %s\n", first);
 	printf("Second string is %s\n\n", second);
-	ft_strncpy(first, second, 2);
+	printf("ft_strncpy returned %s\n\n", strncpy(first, second, 6));
 	printf("First string is %s\n", first);
 	printf("Second string is %s\n\n", second);
 	return 0;
