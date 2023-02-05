08a69f02a97b0b3eadde592c180d17207f14e4f2
lgaudin <lgaudin@student.42malaga.com>
2023-02-05 13:00:32 +0100

    Add ex00 and mains folder

--- c02/mains/ex00/ft_strcpy.c
+++ c02/mains/ex00/ft_strcpy.c
@@ -6,11 +6,12 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 10:55:08 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 12:59:42 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/05 13:00:05 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 #define MAX 2048
 
 char	*ft_strcpy(char *dest, char *src)
@@ -25,4 +26,16 @@ char	*ft_strcpy(char *dest, char *src)
 	}
 	dest[i] = src[i];
 	return (dest);
+}
+
+int main(int argc, char const *argv[])
+{
+	char first[] = "Hello";
+	char second[] = "Bonjour";
+	printf("First string is %s\n", first);
+	printf("Second string is %s\n\n", second);
+	ft_strcpy(first, second);
+	printf("First string is %s\n", first);
+	printf("Second string is %s\n\n", second);
+	return 0;
 }
