2758060b46ad81fbc87664c012b84d3c864feb22
lgaudin <lgaudin@student.42malaga.com>
2023-02-06 17:52:21 +0100

    Add ex01

---
+++ c04/mains/ex01/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/06 17:50:29 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/06 17:51:57 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int main(int argc, char const *argv[])
+{
+	ft_putstr("Bonjour");
+	return 0;
+}
