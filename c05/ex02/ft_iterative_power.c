83d26ec133f7992faa61aea1ec8f8a6d6f29c023
lgaudin <lgaudin@student.42malaga.com>
2023-02-07 17:52:45 +0100

    Add corrections

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:18:38 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/07 17:23:55 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/07 17:52:18 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,12 @@ int	ft_iterative_power(int nb, int power)
 	int	i;
 	int	result;
 
-	if (nb == 0 && power == 0)
+	if (power == 0)
 		return (1);
+	else if (power == 1)
+		return (nb);
+	else if (power == -1)
+		return (1/nb);
 	i = 0;
 	result = 1;
 	while (i++ < power)
--- c05/mains/ex02/ft_iterative_power.c
+++ c05/mains/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:18:38 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/07 17:23:50 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/07 17:52:32 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,12 @@ int	ft_iterative_power(int nb, int power)
 	int	i;
 	int	result;
 
-	if (nb == 0 && power == 0)
+	if (power == 0)
 		return (1);
+	else if (power == 1)
+		return (nb);
+	else if (power == -1)
+		return (1/nb);
 	i = 0;
 	result = 1;
 	while (i++ < power)
