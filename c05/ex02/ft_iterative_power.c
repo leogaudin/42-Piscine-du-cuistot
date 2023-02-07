312f3ae5ea6c8bf83ae53ab1a8e0ea089b7dc9c9
lgaudin <lgaudin@student.42malaga.com>
2023-02-07 17:24:03 +0100

    Add corrections

--- c05/ex02/ft_iterative_power.c
+++ c05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:18:38 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/07 17:22:22 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/07 17:23:55 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@ int	ft_iterative_power(int nb, int power)
 	int	i;
 	int	result;
 
-	if (power == 0)
-		return (0);
+	if (nb == 0 && power == 0)
+		return (1);
 	i = 0;
 	result = 1;
 	while (i++ < power)
