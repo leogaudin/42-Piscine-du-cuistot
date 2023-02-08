cf22761e89743669d26134de649799291eaf3664
lgaudin <lgaudin@student.42malaga.com>
2023-02-08 15:13:58 +0100

    Add corrections

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,62 +6,35 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 18:00:20 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/08 12:55:58 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/08 15:13:30 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	flip_bool(int *bool)
-{
-	if (*bool == 0)
-		*bool = 1;
-	else if (*bool == 1)
-		*bool = 0;
-	else
-		*bool = 0;
-}
-
-int	is_space(char *c)
-{
-	if (*c == ' ')
-		return (1);
-	else if (*c == '\f')
-		return (1);
-	else if (*c == '\n')
-		return (1);
-	else if (*c == '\r')
-		return (1);
-	else if (*c == '\t')
-		return (1);
-	else if (*c == '\v')
-		return (1);
-	else
-		return (0);
-}
-
 int	ft_atoi(char *str)
 {
-	int	returned_number;
-	int	is_negative;
-	int	i;
+	int		i;
+	int		negative;
+	int		result;
 
 	i = 0;
-	returned_number = 0;
-	is_negative = 0;
-	while (str[i])
+	negative = 1;
+	result = 0;
+	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n'
+		|| str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
 	{
-		if (is_space(&str[i]) == 1 || str[i] == '+')
-			i++;
-		else if (str[i] == '-')
-		{
-			flip_bool(&is_negative);
-			i++;
-		}
-		else if (str[i] >= '0' && str[i] <= '9')
-			returned_number = (returned_number * 10) + (str[i++] - '0');
-		else
-			break ;
+		i++;
 	}
-	if (is_negative == 1)
-		returned_number *= -1;
-	return (returned_number);
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			negative *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	result *= negative;
+	return (result);
 }
