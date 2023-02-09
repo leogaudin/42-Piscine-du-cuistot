5478448b57c06a74ce44d76f1a4cdf5aaa9f6852
Léo <leogaudinoff@gmail.com>
2023-02-09 19:19:11 +0100

    Add ex02

---
+++ c06/ex02/ft_rev_params.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 19:16:05 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/09 19:18:54 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int	i;
+	int	length;
+
+	i = argc - 1;
+	length = 0;
+	while (i > 0)
+	{
+		while (argv[i][length])
+			length++;
+		write(1, argv[i], length);
+		write(1, "\n", 1);
+		length = 0;
+		i--;
+	}
+	return (0);
+}
