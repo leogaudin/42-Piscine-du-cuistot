1725ead9c6b01af8f7f0e10121155b9e3a7b3a02
lgaudin <lgaudin@student.42malaga.com>
2023-02-09 15:01:40 +0100

    Add ex06

---
+++ c05/mains/ex06/ft_is_prime.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_prime.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 14:39:18 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/09 15:01:08 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	is_prime;
+	int	i;
+
+	if (nb == 0 || nb == 1)
+		is_prime = 0;
+	else
+		is_prime = 1;
+	i = 2;
+	while (i <= nb / 2)
+	{
+		i++;
+		if (nb % i == 0)
+		{
+			is_prime = 0;
+			break ;
+		}
+	}
+	return (is_prime);
+}
+
+int main(int argc, char const *argv[])
+{
+	int number = 7;
+	printf("Is %d prime? %d\n", number, ft_is_prime(number));
+	return 0;
+}
