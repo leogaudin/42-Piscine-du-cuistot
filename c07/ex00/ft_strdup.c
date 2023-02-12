6f4401d32914b4b960fdccd4c112be9b0ebbc9b0
Léo <leogaudinoff@gmail.com>
2023-02-12 16:46:53 +0100

    Add ex00

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/12 16:12:22 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/12 16:45:58 by lgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		length;
+	char	*copy;
+
+	length = 0;
+	while (src[length])
+		length++;
+	copy = malloc(sizeof(char) * length);
+	copy = src;
+	return (copy);
+}
