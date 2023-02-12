0e81a353339ae63e8e31da4c001ac5fe6058bb94
Léo <leogaudinoff@gmail.com>
2023-02-12 14:37:38 +0100

    Add ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,12 +6,57 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 20:33:50 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/10 11:26:02 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/12 14:37:23 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char const *argv[])
+#include <unistd.h>
+
+void	ft_putstr(char *str)
 {
-	/* code */
-	return 0;
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] > s2[i] || s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		else
+			i++;
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+{
+	int		i;
+	char	*tmp;
+
+	i = 0;
+	while (++i < argc - 1)
+	{
+		if (ft_strcmp(argv[i], argv[i + 1]) > 0)
+		{
+			tmp = argv[i];
+			argv[i] = argv[i + 1];
+			argv[i + 1] = tmp;
+			i = 0;
+		}
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i++]);
+		ft_putstr("\n");
+	}
+	return (0);
 }
