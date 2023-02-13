3716a79a475b5eaf2634dad018203e57c59a3341
Léo <leogaudinoff@gmail.com>
2023-02-13 08:53:15 +0100

    Add new approach to ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,50 +6,42 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:58:30 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/05 18:31:35 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/13 08:52:14 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strlowcase(char *str)
+int	ft_str_is_numeric(char str)
 {
-	int	size;
+	return (str >= '0' && str <= '9');}
 
-	size = 0;
-	while (*str)
-	{
-		if (*str >= 'A' && *str <= 'Z')
-			*str += 32;
-		str++;
-		size++;
-	}
-	str -= size;
-	return (str);
+int	ft_str_is_lowercase(char str)
+{
+	return (str >= 'a' && str <= 'z');
+}
+
+int	ft_str_is_uppercase(char str)
+{
+	return (str >= 'A' && str <= 'Z');
 }
 
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
+	int		i;
+	char	character;
 
 	i = 0;
-	ft_strlowcase(str);
-	if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
+	if (str[i] && ft_str_is_lowercase(str[i]))
+		str[i++] -= 32;
 		i++;
 	while (str[i])
 	{
-		if (!(str[i] >= 'a' && str[i] <= 'z'))
-		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				if (str[i] >= '0' && str[i] <= '9')
-					i++;
-				else
-				{
-					str[i + 1] -= 32;
-					i++;
-				}
-			}
-		}
+		character = str[i - 1];
+		if (ft_str_is_lowercase(character) || ft_str_is_uppercase(character) || ft_str_is_numeric(character))
+			if (ft_str_is_uppercase(str[i]))
+				str[i] += 32;
+		else
+			if (ft_str_is_lowercase(str[i]))
+				str[i] -= 32;
 		i++;
 	}
 	return (str);
