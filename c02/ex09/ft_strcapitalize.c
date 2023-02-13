33bea53267fa946d64a2fedb2980c754b5f714b4
Léo <leogaudinoff@gmail.com>
2023-02-13 19:01:00 +0100

    Add ANOTHER potential solution

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,48 +6,30 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 13:58:30 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/13 08:56:22 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/13 19:00:51 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_str_is_numeric(char str)
-{
-	return (str >= '0' && str <= '9');
-}
-
-int	ft_str_is_lowercase(char str)
-{
-	return (str >= 'a' && str <= 'z');
-}
-
-int	ft_str_is_uppercase(char str)
-{
-	return (str >= 'A' && str <= 'Z');
-}
-
 char	*ft_strcapitalize(char *str)
 {
-	int		i;
-	char	character;
+	unsigned int	i;
 
-	i = 0;
-	if (str[i] && ft_str_is_lowercase(str[i]))
-		str[i++] -= 32;
-		i++;
-	while (str[i])
+	if (str[0] != '\0' && str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+	i = 1;
+	while (str[i] != '\0')
 	{
-		character = str[i - 1];
-		if (ft_str_is_lowercase(character) || ft_str_is_uppercase(character)
-			|| ft_str_is_numeric(character))
+		if (((str[i] >= 'a' && str[i] <= 'z') ||
+					(str[i] >= 'A' && str[i] <= 'Z')) && (str[i - 1] < '0' ||
+					(str[i - 1] > '9' && str[i - 1] < 'A') ||
+					(str[i - 1] > 'Z' && str[i - 1] < 'a') ||
+					str[i - 1] > 'z'))
 		{
-			if (ft_str_is_uppercase(str[i]))
-				str[i] += 32;
-		}
-		else
-		{
-			if (ft_str_is_lowercase(str[i]))
+			if (str[i] >= 'a' && str[i] <= 'z')
 				str[i] -= 32;
 		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
 		i++;
 	}
 	return (str);
