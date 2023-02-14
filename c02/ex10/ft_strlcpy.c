4ded818714b5d4241f1ab582c5566e964a10cf58
Léo <leogaudinoff@gmail.com>
2023-02-14 07:50:50 +0100

    Add different approach for ex10

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,26 +6,27 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 14:52:31 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/13 08:57:56 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/14 07:50:35 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-	unsigned int	count;
+	unsigned	int	i;
+	unsigned	int	j;
 
 	i = 0;
-	count = 0;
-	while (src[count])
-		count++;
-	if (count == 0)
-		return (0);
-	while (src[i] && i < (size - 1))
-	{
-		dest[i] = src[i];
+	while (dest[i])
 		i++;
+	j = 0;
+	while (src[j] && j < size)
+	{
+		dest[i + j] = src[j];
+		j++;
 	}
-	dest[i] = '\0';
-	return (count);
+	dest[i + j] = '\0';
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
 }
