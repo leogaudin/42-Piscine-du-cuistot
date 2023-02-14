46cbb37b359237db20246986d447117b7468b88c
Léo <leogaudinoff@gmail.com>
2023-02-14 10:45:03 +0100

    Add new approach for iterative factorial

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,23 +6,23 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:04:33 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/13 15:05:12 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/14 10:44:51 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
 	int	result;
-	int	i;
 
-	if (nb < 0 || nb >= 13)
-		return (0);
 	result = 1;
-	i = nb;
-	while (i >= 1)
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	while (nb > 0)
 	{
-		result *= i;
-		i--;
+		result *= nb;
+		nb--;
 	}
 	return (result);
 }
--- c05/mains/ex00/ft_iterative_factorial.c
+++ c05/mains/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 17:04:33 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/13 15:05:37 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/14 10:44:44 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 int	ft_iterative_factorial(int nb)
 {
 	int	result;
-	int	i;
 
-	if (nb < 0 || nb >= 13)
-		return (0);
 	result = 1;
-	i = nb;
-	while (i >= 1)
+	if (nb < 0)
+		return (0);
+	if (nb <= 1)
+		return (1);
+	while (nb > 0)
 	{
-		result *= i;
-		i--;
+		result *= nb;
+		nb--;
 	}
 	return (result);
 }
