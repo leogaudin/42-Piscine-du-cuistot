a96297179bf49ae5bf0596804e9dabdd42bc8248
lgaudin <lgaudin@student.42malaga.com>
2023-02-16 19:46:48 +0100

    Trying things

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:30:01 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/16 13:41:57 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/16 19:08:42 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ char	**ft_split(char *str, char *charset)
 {
 	char	**result;
 	int		result_index;
+	int		string_index;
+	int		charset_index;
 	int		i;
 
 	i = 0;
@@ -23,5 +25,30 @@ char	**ft_split(char *str, char *charset)
 		i++;
 	result = malloc(sizeof(char) * i + i - 1);
 	result_index = 0;
-
+	string_index = 0;
+	charset_index = 0;
+	i = 0;
+	while (str[i])
+	{
+		while (charset[charset_index])
+		{
+			if (str[i] != charset[charset_index])
+			{
+				result[result_index][string_index] = str[i];
+				string_index++;
+				i++;
+			}
+			else
+			{
+				result_index++;
+				i++;
+				string_index = 0;
+			}
+			charset_index++;
+		}
+		charset_index = 0;
+		i++;
+	}
+	result[result_index] = 0;
+	return (result);
 }
