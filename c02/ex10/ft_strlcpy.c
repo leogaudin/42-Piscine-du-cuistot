e569b43d5975bedd25167843e4d3fab2c5da80f3
lgaudin <lgaudin@student.42malaga.com>
2023-02-16 19:10:14 +0100

    Add correction

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,24 +6,26 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 14:52:31 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/14 14:01:07 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/16 19:10:02 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	count;
 	unsigned int	i;
+	unsigned int	res;
 
-	count = 0;
-	while (src[count])
-		count++;
 	i = 0;
+	res = 0;
+	while (src[res])
+		res++;
+	if (size == 0)
+		return (res);
 	while (src[i] && i < (size - 1))
 	{
 		dest[i] = src[i];
 		i++;
 	}
 	dest[i] = '\0';
-	return (count);
+	return (res);
 }
