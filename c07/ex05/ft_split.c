f1aba10449013c499f431d4eaea86312195fc98c
lgaudin <lgaudin@student.42malaga.com>
2023-02-18 17:56:49 +0100

    Add bullshit I don't know what I'm doing anymore

--- c07/ex05/ft_split.c
+++ c07/ex05/ft_split.c
@@ -6,55 +6,12 @@
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:30:01 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/18 17:51:05 by lgaudin          ###   ########.fr       */
+/*   Updated: 2023/02/18 17:56:32 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
-
-char	**ft_split(char *str, char *charset)
-{
-	char	**result;
-	int		result_index;
-	int		string_index;
-	int		charset_index;
-	int		i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	result = malloc(sizeof(char) * i + i - 1);
-	result_index = 0;
-	string_index = 0;
-	charset_index = 0;
-	i = 0;
-	while (str[i])
-	{
-		result[result_index][string_index++] = str[i++];
-		// while (charset[charset_index])
-		// {
-		// 	if (str[i] != charset[charset_index])
-		// 	{
-		// 		result[result_index][string_index] = str[i];
-		// 		string_index++;
-		// 		i++;
-		// 	}
-		// 	else
-		// 	{
-		// 		result_index++;
-		// 		i++;
-		// 		string_index = 0;
-		// 	}
-		// 	charset_index++;
-		// }
-		// charset_index = 0;
-		// i++;
-	}
-	result_index++;
-	result[result_index] = 0;
-	return (result);
-}
 
 /**
  * @brief
@@ -94,11 +51,54 @@ int	count_strings(char *str, char *charset)
 	return (strings_count);
 }
 
+char	**ft_split(char *str, char *charset)
+{
+	char	**result;
+	int		result_index;
+	int		string_index;
+	int		charset_index;
+	int		i;
+
+	i = 0;
+	result = malloc(sizeof(char *));
+	while (i < count_strings(str, charset))
+		result[i] = malloc(sizeof(char) * 2048);
+	result_index = 0;
+	string_index = 0;
+	charset_index = 0;
+	i = 0;
+	while (str[i])
+	{
+		result[result_index][string_index++] = str[i++];
+		while (charset[charset_index])
+		{
+			if (str[i] != charset[charset_index])
+			{
+				result[result_index][string_index] = str[i];
+				string_index++;
+				i++;
+			}
+			else
+			{
+				result_index++;
+				i++;
+				string_index = 0;
+			}
+			charset_index++;
+		}
+		charset_index = 0;
+		i++;
+	}
+	result_index++;
+	result[result_index] = 0;
+	return (result);
+}
+
 int main(int argc, char const *argv[])
 {
 	char str[] = "Salut, j'espère que ça va, moi top. Bonjourno....";
 	char charset[] = " ";
-	// char **result = ft_split(str, charset);
 	printf("There are %d strings in \"%s\"\n", count_strings(str, charset), str);
+	char **result = ft_split(str, charset);
 	return 0;
 }
