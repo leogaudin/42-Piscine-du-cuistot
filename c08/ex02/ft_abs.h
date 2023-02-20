0100abf4c8892f6ead55c162bd7478fa62ba7742
lgaudin <lgaudin@student.42malaga.com>
2023-02-20 11:41:43 +0100

    Add exercises for C08

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   ft_abs.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgaudin <lgaudin@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 18:27:28 by lgaudin           #+#    #+#             */
-/*   Updated: 2023/02/20 11:39:13 by lgaudin          ###   ########.fr       */
+/*   Created: 2023/02/19 12:42:45 by lgaudin           #+#    #+#             */
+/*   Updated: 2023/02/19 12:43:37 by lgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
+#ifndef FT_ABS_H
+# define FT_ABS_H
 
-# include <unistd.h>
-
-# define FT_BOOLEAN_H
-
-# define TRUE 1
-# define FALSE 0
-# define EVEN(nbr) (nbr + 1) % 2
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-# define SUCCESS 0
-
-typedef int	t_bool;
+# define ABS(nbr) (((nbr) < 0) ? -(nbr) : (nbr))
 
 #endif
